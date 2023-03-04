4eb7f19087c280b2213eef13cf5904e26d80a709
stickkman <stickkman@gmail.com>
2023-03-03 21:16:47 -0500

    coded seeds section, all work except tripData(needs json filled out) and imageData(needs mimetype and data fields filled out), they are all coded just need to uncomment those 2 specific sections once jsons filled out.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,56 @@
 const sequelize = require('../config/connection');
-const { User,Traveller,image,destinations } = require('../models');
+const { User, Traveller, image, destinations, trips} = require('../models');
 
 const destinationData = require('./destinationData.json');
 const travellerData = require('./travellerData.json');
+const imageData = require('./imageData.json');
+// const tripsData = require('./tripsData.json'); // !!!! UNCOMMENT AFTER DATA IS IN TRIPSDATA.JSON !!!!
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
-  try{
-    await sequelize.sync({ force: true });
-      await destinations.bulkCreate(destinationData, {
+  try { 
+    await sequelize.sync({ force: true }); // sync the sequelize models w database
+    
+    // seed destinations table
+    await destinations.bulkCreate(destinationData, {
+      individualHooks: true,
+      returning: true, 
+    });
+    
+    // seed travellers table
+    await Traveller.bulkCreate(travellerData, {
+        individualHooks: true,
+        returning: true,
+    });
+    //                                     !!!! UNCOMMENT AFTER IMAGE MODEL HAS mimetype and data fields filled in !!!!
+    // // seed images table
+    // await image.bulkCreate(imageData, {
+    //       individualHooks: true,
+    //       returning: true,
+    // });
+
+    // // seed trips table                 !!!! UNCOMMENT AFTER DATA IS ENTERED into TRIPSDATA.JSON !!!!
+    // await trips.bulkCreate(tripsData, {
+    //       individualHooks: true,
+    //       returning: true,
+    // }); 
+    
+    // seed user table
+    await User.bulkCreate(userData, {
       individualHooks: true,
       returning: true,
-    });
-    // await here
-    // bulk create more models await syntax.. one after another
+    }); 
+
+    console.log('-------Database Seeding Complete-------')
+    
     process.exit(0);
-  } catch(err){
+
+  } catch(err) {
     console.log(err)
   }
+};
 
-  // 
 
-  
-  
 
 
   // for (const project of projectData) {
@@ -31,6 +60,6 @@ const seedDatabase = async () => {
   //   });
   // }
 
-};
+
 
 seedDatabase();
